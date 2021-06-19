82b2fac73d862a62432faeabfc0bb6a66c520d06
Stefan Kruppa <stefan.kruppa@reservix.de>
2021-06-19 14:57:58 +0200

    Init

---
+++ src/Test/Class1.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Test;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class Class1
+{
+    public function method1()
+    {
+    }
+
+    protected function method2(): void
+    {
+    }
+
+    protected function method3(): bool
+    {
+    }
+
+    protected function method4(): array
+    {
+    }
+
+    protected function method5(Request $request)
+    {
+    }
+
+    protected function method6(): string
+    {
+    }
+
+    private function method7(): string
+    {
+    }
+
+    private function method8(): int
+    {
+    }
+
+    public function method9(): array
+    {
+    }
+
+    private function method10(): array
+    {
+    }
+
+    private function method11(): void
+    {
+    }
+
+    private function method12(): string
+    {
+    }
+
+    private function method13(Request $request): void
+    {
+    }
+
+    private function method14(): void
+    {
+    }
+
+    private function method15(): void
+    {
+    }
+
+    private function method16(): int
+    {
+    }
+
+    private function method17(): string
+    {
+    }
+
+    private function method18(): void
+    {
+    }
+
+    private function method19(): array
+    {
+    }
+
+    private function method20(int $id)
+    {
+    }
+
+    private function method21(): void
+    {
+    }
+
+    private function method22(): void
+    {
+    }
+
+    private function method23(Request $request): void
+    {
+    }
+
+    private function method24(Request $request): void
+    {
+    }
+}
